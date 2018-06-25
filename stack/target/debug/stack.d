675dba2c3cc78631dc5b5ae71f415e617f0ea51a
mercury <udtrokia@gmail.com>
2018-06-25 23:12:15 +0800

    inital commit

--- stack/target/debug/stack.d
+++
@@ -1,1 +0,0 @@
-/Users/mercury/Code/self/dast/stack/target/debug/stack.dSYM: /Users/mercury/Code/self/dast/stack/src/lib.rs /Users/mercury/Code/self/dast/stack/src/main.rs /Users/mercury/Code/self/dast/stack/src/stack.rs
